e50ae7ab2e43bc1f4fbfb9e0d3791f4860dc967c
Zach <zdb1994@yahoo.com>
2020-08-26 21:25:28 -0400

    Modifying description one last time.

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'A software development team with a passionate to write beautiful software to help lives.' }
+      { hid: 'description', name: 'description', content: 'A software development team with a passionate to write beautiful software to help lives.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
